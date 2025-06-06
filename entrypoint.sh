8c477b8143f059c3aae5d17d259bd02bbdaa05e2
misswird <148736979+misswird@users.noreply.github.com>
2025-06-06 10:07:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -324,7 +324,7 @@ check_variable() {
 # 下载最新版本 Nezha Agent
 download_agent() {
   if [ ! -e nezha-agent ]; then
-    URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
+    URL=\$(wget -qO- -4 "https://github.com/nezhahq/agent/releases/download/v0.17.5/nezha-agent_freebsd_amd64.zip" | grep -o "https.*linux_amd64.zip")
     wget -t 2 -T 10 -N \${URL}
     unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
   fi
